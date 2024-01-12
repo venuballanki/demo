393f45e135642ccbcfa82c85a8cd10af55442eb9
venuballanki <146913865+venuballanki@users.noreply.github.com>
2024-01-12 10:19:16 +0530

    sample code

---
+++ venu.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://www.google.com/imgres?imgurl=https%3A%2F%2Fdogtime.com%2Fwp-content%2Fuploads%2Fsites%2F12%2F2023%2F10%2FGettyImages-1037524792-e1696604393664.jpg%3Fw%3D1024&tbnid=YGcULQL24wKUvM&vet=12ahUKEwiHp6bZhdeDAxVDq2MGHeCkBT0QMygEegQIARBZ..i&imgrefurl=https%3A%2F%2Fdogtime.com%2Fdog-breeds%2Ftibetan-mastiff&docid=nxLHZrKD_OUXEM&w=1024&h=638&q=tebatian%20mastiff&ved=2ahUKEwiHp6bZhdeDAxVDq2MGHeCkBT0QMygEegQIARBZ',caption='puppy')
